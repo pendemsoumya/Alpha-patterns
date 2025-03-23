a5ddcad5699153f5c3e1b2f8b38f8a11900515cf
pendemsoumya <166056253+pendemsoumya@users.noreply.github.com>
2025-03-23 22:19:54 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,11 @@
+class program{
+public static void main(String args[]){
+for(char i ='A' ; i<='E'; i++){
+for(char j ='A'; j<='E'; j++){
+System.out.print(i);
+}
+System.out.println("\n");
+}
+}
+}
+
---
+++ program2.java
@@ -0,0 +1,12 @@
+class program2{
+public static void main(String args[]){
+char i;
+char j;
+for(i ='A'; i<='E'; i++){
+for(j = 'A'; j<=i; j++){
+System.out.print(i);
+}
+System.out.println();
+}
+}
+}
